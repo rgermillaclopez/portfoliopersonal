ef59c9bc39d545957d33629757d3f7a342c35344
Rocio <rgermillaclopez@gmail.com>
2023-01-03 20:06:06 +0100

    corregir la posicion de los iconos de redes sociales

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const typed = new Typed('.typed', {
-    strings: ['GIL', 'BOLUDAZO', ';)']
+    strings: ['WE', 'WORK', 'TOGETHER?'
+    ],
+
+        //stringElement: '#cadenas-texto',
+        typeSpeed: 80,
+        startDelay: 300,
+        backSpeed: 75,
+        //smartBackspace: true,
+        shuffle: false,
+        backDelay: 500,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: '|',
+        contentType: 'html',
+
+
     }
 );
